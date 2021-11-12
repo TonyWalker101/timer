a467811e5d1661c4b25b029278c00cf2161da3c7
Tony <awalker780@gmail.com>
2021-11-11 22:59:59 -0500

    Created timer function

---
+++ timer1.js
@@ -0,0 +1,22 @@
+// For every input, alarm will beep after the input's value in seconds
+
+const process = require("process");
+
+// organizes array into only the inputted elements in ascending order
+const args = process.argv.slice(2).sort();
+
+// Helper function that calls the setTimeout function
+const alarm = num => {
+  return setTimeout(() => {
+    process.stdout.write('\x07');
+  }, num * 1000);
+};
+
+for (let i = 0; i < args.length; i++) {
+  
+  if (args[i] < 0 || !Number.isInteger(parseInt(args[i]))) {
+    continue;
+  }
+
+  alarm(args[i]);
+}
